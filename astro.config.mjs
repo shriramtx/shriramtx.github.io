5eb5a893292bbeb6f45d5031b7c7ee1e45f2bd4b
Shriram <shriramspark@gmail.com>
2024-02-26 15:57:44 +0530

    Dante Push

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://shriramtx.github.io',
     integrations: [
         mdx(),
         sitemap(),
